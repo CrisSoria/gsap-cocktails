cfc67f3ebe0fdb80e7e43df7f547924b5a05e133
Cristian Soria <crissoria07@gmail.com>
2025-06-22 14:20:54 -0300

    implement navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {gsap} from "gsap";
 import {ScrollTrigger, SplitText} from "gsap/all";
+import Navbar from "./components/Navbar.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText); //con esto queda habilitado el uso global
 
 const App = () => {
     return (
-        <div className="flex-center h-[100vh]">
-            <h1 className="text-3xl text-indigo-300"> Probando ando </h1>
-        </div>
+        <main className="App">
+            <Navbar/>
+        </main>
     );
 };
 
